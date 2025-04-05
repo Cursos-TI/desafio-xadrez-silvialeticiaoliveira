27a0e40625ff84bfeb8dc1d832a50de7d0394ec2
silvialeticiaoliveira <leticia.minecon@gmail.com>
2025-04-05 19:44:19 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -4,9 +4,10 @@ int main() {
     // INICIO:
     // DECLARAÇÃO DE VARIAVEIS:
     // i para a primeira estrutura de repetição, j para o FOR e k para do...while:
-    int i = 1, j = 1, k = 1;
+    int i = 1, j = 1, k = 1, m = 0;
 
 
+    
     // PROCESSAMENTO:
     // Utilizando while para mostrar a movimentação da torre, cinco casas pra direita:
     while (i <= 5)
@@ -27,8 +28,24 @@ int main() {
         printf("Rainha avançou para esquerda\n");
     } while(k <= 8);
     
+    // declaração da variavel local L, dentro do proprio 'for':
+    for (int l = 0; l < 1; l++)
+    {
+        // printf do lado de fora do while:
+        printf("Cavalo avançou para ");
 
-
+        // utilizei o loop while para exibir a palavra 'baixo' que aparece duas vezes:
+        while (m <= 1)
+        {
+            printf("baixo, ");
+            m++;
+        }
+        
+        // printf novamente do lado de fora:
+        printf("esquerda\n");
+        
+    }
+    
     // FINAL:
     return 0;
 }
