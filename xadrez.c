e6032e7c263f92c8486c6804384e9719063282c2
silvialeticiaoliveira <leticia.minecon@gmail.com>
2025-04-06 23:29:50 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,48 +1,70 @@
 #include <stdio.h>
 
+// declarações de procedimentos fora da função principal:
+// Procedimento pra torre:
+void torre_avanca(int i) {
+    // Condição de parada:
+    if (i > 5) {
+        return;
+    }
+
+    printf("Torre avançou para direita\n");
+    torre_avanca(i + 1); // condição pra recursividade:
+}
+
+// Procedimento pra rainha:
+void rainha_avanca(int j){
+    // Condição de parada:
+    if(j > 8) {
+        return;
+    }
+
+    printf("Rainha avançou para esquerda\n");
+    rainha_avanca(j + 1); // condição pra recursividade:
+}
+
+
+
+// Inicio da função principal:
 int main() {
-    // INICIO:
-    // DECLARAÇÃO DE VARIAVEIS:
-    // i para a primeira estrutura de repetição, j para o FOR e k para do...while:
-    int i = 1, j = 1, k = 1, m = 0;
+    // INICIO com variaveis locais declaradas dentro do seu respectivo bloco de comando:
 
-
-    
     // PROCESSAMENTO:
-    // Utilizando while para mostrar a movimentação da torre, cinco casas pra direita:
-    while (i <= 5)
+    // chamando a função de movimentação da torre e atribuindo um valor inicial:
+    torre_avanca(1);
+
+    // Utilizando LOOPS ANINHADOS para mostrar a movimentação do bispo:
+    for (int k = 1, l = 1; k <= 5; k++)
     {
-        i++;
-        printf("Torre avançou para direita\n");
+
+        printf("Bispo avançou para cima, ");
+        do{
+            l++;
+            printf("direita");
+
+        } while (l <= 1);
+        printf("\n");
+
     }
-    
-    // Utilizando FOR para mostrar a movimentação do bispo:
-    for (; j <= 5; j++)
+
+    // chamando a função de movimentação da rainha e atribuindo um valor inicial:
+    rainha_avanca(1);
+
+    // loop aninhado complexo para a movimentação do cavalo:
+    for (int m = 0, o = 0; m < 1; m++)
     {
-        printf("Bispo avançou para cima, direita\n");
-    }
-    
-    // Utilizando DO....WHILE para mostrar a movimentação da rainha:
-    do{
-        k++;
-        printf("Rainha avançou para esquerda\n");
-    } while(k <= 8);
-    
-    // declaração da variavel local L, dentro do proprio 'for':
-    for (int l = 0; l < 1; l++)
-    {
-        // printf do lado de fora do while:
+        // printf do lado de fora do while pra ser executado apenas uma vez:
         printf("Cavalo avançou para ");
 
-        // utilizei o loop while para exibir a palavra 'baixo' que aparece duas vezes:
-        while (m <= 1)
+        // utilizei o loop while para exibir a palavra 'cima' que aparece duas vezes:
+        while (o <= 1)
         {
-            printf("baixo, ");
-            m++;
+            printf("cima, ");
+            o++;
         }
         
-        // printf novamente do lado de fora:
-        printf("esquerda\n");
+        // printf novamente do lado de fora, para imprimir na tela 'direita' uma única vez:
+        printf("direita\n");
         
     }
     
